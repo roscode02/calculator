a9c4a404e76fad634be0ba8d9eff34a3c86a9d10
Harsh <patelharsh0603@gmail.com>
2021-04-05 20:49:39 +0530

    Used the kotlin scope functions for binding.

--- app/src/main/java/com/example/calculator/StartFragment.kt
+++ app/src/main/java/com/example/calculator/StartFragment.kt
@@ -2,30 +2,37 @@ package com.example.calculator
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.Navigation
 import com.example.calculator.databinding.FragmentStartBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
 
-   class StartFragment : Fragment(R.layout.fragment_start){
-     private lateinit var binding: FragmentStartBinding
-      override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-         super.onViewCreated(view, savedInstanceState)
-         binding = FragmentStartBinding.bind(view)
 
-         binding.button1.setOnClickListener{
-          Navigation.findNavController(view).navigate(R.id.action_startFragment2_to_firstFragment)
+class StartFragment : Fragment(R.layout.fragment_start) {
+    private lateinit var binding: FragmentStartBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentStartBinding.bind(view)
 
-         }
+        binding.apply {
 
-         binding.straightbutton.setOnClickListener{
-            Navigation.findNavController(view).navigate(R.id.action_startFragment2_to_secondFragment)
-         }
-      }
-   }
+
+            button1.setOnClickListener {
+                Navigation.findNavController(view)
+                    .navigate(R.id.action_startFragment2_to_firstFragment)
+
+            }
+
+            straightbutton.setOnClickListener {
+                Navigation.findNavController(view)
+                    .navigate(R.id.action_startFragment2_to_secondFragment)
+            }
+
+
+            //Any new binding. should be written here
+
+        }
+    }
+}
 
